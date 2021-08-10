e28402cc666bbd3029ac005c5cee4f77d4294367
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-08-10 23:43:04 +0900

    reduxToolkit(Nomard Corder)

---
+++ redux-toolkit/src/routes/Home.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { add } from '../store';
+import ToDo from '../components/ToDo';
+function Home({ toDos, addToDo }) {
+  const [text, setText] = useState('');
+  function onChange(e) {
+    setText(e.target.value);
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+    addToDo(text);
+    setText('');
+  }
+  return (
+    <React.Fragment>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo {...toDo} key={toDo.id} />
+        ))}
+      </ul>
+    </React.Fragment>
+  );
+}
+
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: (text) => dispatch(add(text)),
+  };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
