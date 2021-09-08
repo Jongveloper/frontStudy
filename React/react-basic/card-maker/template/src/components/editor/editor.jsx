6b862de0fb32c1f776e982acb877002c9610f5a7
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-09-08 12:06:40 +0900

    ✨이미지 업로드 기능 구현

--- React/react-basic/card-maker/template/src/components/editor/editor.jsx
+++ React/react-basic/card-maker/template/src/components/editor/editor.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import styles from './editor.module.css';
 import CardEditForm from '../card_edit_form/card_edit_form';
 import CardAddForm from '../card_add_form/card_add_form';
-const Editor = ({ cards, addCard }) => (
+const Editor = ({ FileInput, cards, addCard, updateCard, deleteCard }) => (
   <section className={styles.editor}>
     <h1 className={styles.title}>Card Maker</h1>
-    {cards.map((card) => (
-      <CardEditForm key={card.id} card={card} />
+    {Object.keys(cards).map((key) => (
+      <CardEditForm
+        key={key}
+        FileInput={FileInput}
+        card={cards[key]}
+        updateCard={updateCard}
+        deleteCard={deleteCard}
+      />
     ))}
-    <CardAddForm onAdd={addCard} />
+    <CardAddForm FileInput={FileInput} onAdd={addCard} />
   </section>
 );
 
--- React/react-basic/card-maker/template/src/components/maker/maker.jsx
+++ React/react-basic/card-maker/template/src/components/maker/maker.jsx
@@ -5,9 +5,9 @@ import Footer from '../footer/footer';
 import Editor from '../editor/editor';
 import Preview from '../preview/preview';
 import { useHistory } from 'react-router-dom';
-const Maker = ({ authService }) => {
-  const [cards, setCards] = useState([
-    {
+const Maker = ({ FileInput, authService }) => {
+  const [cards, setCards] = useState({
+    1: {
       id: '1',
       name: 'jong',
       company: 'code',
@@ -18,7 +18,7 @@ const Maker = ({ authService }) => {
       fileName: 'jong',
       fileURL: null,
     },
-    {
+    2: {
       id: '2',
       name: 'chu',
       company: 'code',
@@ -29,7 +29,7 @@ const Maker = ({ authService }) => {
       fileName: 'chu',
       fileURL: null,
     },
-    {
+    3: {
       id: '3',
       name: 'joo',
       company: 'code',
@@ -40,7 +40,8 @@ const Maker = ({ authService }) => {
       fileName: 'joo',
       fileURL: null,
     },
-  ]);
+  });
+
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -54,15 +55,31 @@ const Maker = ({ authService }) => {
     });
   });
 
-  const addCard = (card) => {
-    const updated = [...cards, card];
-    setCards(updated);
+  const createOrUpdateCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      updated[card.id] = card;
+      return updated;
+    });
+  };
+  const deleteCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      delete updated[card.id];
+      return updated;
+    });
   };
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} addCard={addCard} />
+        <Editor
+          FileInput={FileInput}
+          cards={cards}
+          addCard={createOrUpdateCard}
+          updateCard={createOrUpdateCard}
+          deleteCard={deleteCard}
+        />
         <Preview cards={cards} />
       </div>
       <Footer />
