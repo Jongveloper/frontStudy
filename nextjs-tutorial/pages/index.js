8c392ca0ef160ad7baecd33b6f680bf1ed310956
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-02 18:40:51 +0900

    💄로딩 화면 만들기(코딩 앙마님)

--- nextjs-tutorial/pages/index.js
+++ nextjs-tutorial/pages/index.js
@@ -3,16 +3,19 @@ import Axios from 'axios';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import ItemList from '../src/components/ItemList/ItemList';
-import { Header, Divider } from 'semantic-ui-react';
+import { Header, Divider, Loader } from 'semantic-ui-react';
 
 export default function Home() {
   const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   const API_URL =
     'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
 
   function getData() {
     Axios.get(API_URL).then((res) => {
       setList(res.data);
+      setIsLoading(false);
     });
   }
   useEffect(() => {
@@ -22,17 +25,29 @@ export default function Home() {
     <div>
       <Head>
         <title>HOME | Jongveloper</title>
+        <meta name="description" content="next.js를 연습합니다."></meta>
       </Head>
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        베스트 상품
-      </Header>
-      <Divider />
-      <ItemList list={list.slice(0, 9)} />
-      <Header as="h3" style={{ paddingTop: 40 }}>
-        신상품
-      </Header>
-      <Divider />
-      <ItemList list={list.slice(9)} />
+      {isLoading && (
+        <div style={{ padding: '300px 0' }}>
+          <Loader inline="centered" active>
+            Loading
+          </Loader>
+        </div>
+      )}
+      {!isLoading && (
+        <>
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            베스트 상품
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(0, 9)} />
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            신상품
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(9)} />
+        </>
+      )}
     </div>
   );
 }
