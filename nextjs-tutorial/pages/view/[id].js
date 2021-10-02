75f4b1d19ecb9db324e0ec9353f53ac27ce34154
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-02 18:42:13 +0900

    ♻️ Link 태그 수정 (코딩 앙마님)

--- nextjs-tutorial/pages/view/[id].js
+++ nextjs-tutorial/pages/view/[id].js
@@ -1,28 +1,33 @@
 import axios from 'axios';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import Head from 'next/dist/shared/lib/head';
 import Item from '../../src/components/Item/Item';
 
-const Post = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  console.log(id);
-  const [item, setItem] = useState({});
-  const API_URL = `https://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
-
-  function getData() {
-    axios.get(API_URL).then((res) => {
-      setItem(res.data);
-    });
-  }
-
-  useEffect(() => {
-    if (id && id > 0) {
-      getData();
-    }
-  }, [id]);
-
-  return <Item item={item} />;
+const Post = ({ item }) => {
+  return (
+    <>
+      {item && (
+        <>
+          <Head>
+            <title>{item.name}</title>
+            <meta name="description" content={item.description}></meta>
+          </Head>
+          <Item item={item} />
+        </>
+      )}
+    </>
+  );
 };
 
 export default Post;
+
+export async function getServerSideProps(context) {
+  const id = context.params.id;
+  const apiURL = `https://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  const res = await axios.get(apiURL);
+  const data = res.data;
+  return {
+    props: {
+      item: data,
+    },
+  };
+}
