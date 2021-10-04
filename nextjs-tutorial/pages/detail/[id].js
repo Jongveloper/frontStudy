a96e3201b8850c7809f7b0a5a2ae7d52032408b4
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-04 17:59:26 +0900

    ✨정적생성 (코딩 앙마님)

---
+++ nextjs-tutorial/pages/detail/[id].js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import Head from 'next/dist/shared/lib/head';
+import Item from '../../src/components/Item/Item';
+
+const Post = ({ item, name }) => {
+  return (
+    <>
+      {item && (
+        <>
+          <Head>
+            <title>{item.name}</title>
+            <meta name="description" content={item.description}></meta>
+          </Head>
+          {name} 환경입니다.
+          <Item item={item} />
+        </>
+      )}
+    </>
+  );
+};
+
+export default Post;
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { id: '740' } },
+      { params: { id: '730' } },
+      { params: { id: '729' } },
+    ],
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context) {
+  const id = context.params.id;
+  const apiURL = `https://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  const res = await axios.get(apiURL);
+  const data = res.data;
+  return {
+    props: {
+      item: data,
+      name: process.env.name,
+    },
+  };
+}
--- nextjs-tutorial/pages/index.js
+++ nextjs-tutorial/pages/index.js
@@ -5,48 +5,38 @@ import styles from '../styles/Home.module.css';
 import ItemList from '../src/components/ItemList/ItemList';
 import { Header, Divider, Loader } from 'semantic-ui-react';
 
-export default function Home() {
-  const [list, setList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-  function getData() {
-    Axios.get(API_URL).then((res) => {
-      setList(res.data);
-      setIsLoading(false);
-    });
-  }
-  useEffect(() => {
-    getData();
-  }, []);
+export default function Home({ list }) {
   return (
     <div>
       <Head>
         <title>HOME | Jongveloper</title>
         <meta name="description" content="next.js를 연습합니다."></meta>
       </Head>
-      {isLoading && (
-        <div style={{ padding: '300px 0' }}>
-          <Loader inline="centered" active>
-            Loading
-          </Loader>
-        </div>
-      )}
-      {!isLoading && (
-        <>
-          <Header as="h3" style={{ paddingTop: 40 }}>
-            베스트 상품
-          </Header>
-          <Divider />
-          <ItemList list={list.slice(0, 9)} />
-          <Header as="h3" style={{ paddingTop: 40 }}>
-            신상품
-          </Header>
-          <Divider />
-          <ItemList list={list.slice(9)} />
-        </>
-      )}
+      <>
+        <Header as="h3" style={{ paddingTop: 40 }}>
+          베스트 상품
+        </Header>
+        <Divider />
+        <ItemList list={list.slice(0, 9)} />
+        <Header as="h3" style={{ paddingTop: 40 }}>
+          신상품
+        </Header>
+        <Divider />
+        <ItemList list={list.slice(9)} />
+      </>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const apiURL = process.env.apiURL;
+  const res = await Axios.get(apiURL);
+  const data = res.data;
+
+  return {
+    props: {
+      list: data,
+      name: process.env.name,
+    },
+  };
 }
