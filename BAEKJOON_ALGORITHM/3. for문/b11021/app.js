56e7d4508817a89a82edcd0a500d2823928d8b9e
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-04 13:28:19 +0900

    📝백준 b11021(A+B-7)

---
+++ BAEKJOON_ALGORITHM/3. for문/b11021/app.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+const testCaseArray = [];
+for (let i = 1; i <= +input[0]; ++i) {
+  const tempValue = input[i].split(' ').map((item) => +item);
+  testCaseArray.push(tempValue);
+}
+
+solution(input[0], testCaseArray);
+
+function solution(T, item) {
+  for (let i = 0; i < T; i++) {
+    console.log(`Case #${i + 1}: ${item[i][0] + item[i][1]}`);
+  }
+}
