54c21b4ebeed72fbbb4685b67c7c389cdaf0c29e
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-10 04:43:47 +0900

    📝백준3052(나머지)

---
+++ BAEKJOON_ALGORITHM/5. 배열/b3052/app.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .split('\n')
+  .map((item) => +item);
+
+const userNum = [];
+
+input.forEach((x) => {
+  const num = x % 42;
+
+  if (userNum.indexOf(num) === -1) {
+    userNum.push(num);
+  }
+});
+
+console.log(userNum.length);
