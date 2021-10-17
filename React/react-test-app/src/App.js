1f66ddced758597eea355564ba525f57bc8a1ba4
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-17 15:04:22 +0900

    📝인프런 John Ahn님의 따라하며 배우는 리액트 테스트 강의

--- React/react-test-app/src/App.js
+++ React/react-test-app/src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3 data-testid="counter">{counter}</h3>
+        <div>
+          <button
+            data-testid="minus-button"
+            disabled={disabled}
+            onClick={() => setCounter((count) => count - 1)}
+          >
+            -
+          </button>
+          <button
+            data-testid="plus-button"
+            disabled={disabled}
+            onClick={() => setCounter((count) => count + 1)}
+          >
+            +
+          </button>
+        </div>
+        <div>
+          <button
+            style={{ backgroundColor: 'blue' }}
+            data-testid="on/off-button"
+            onClick={() => setDisabled((prev) => !prev)}
+          >
+            on/off
+          </button>
+        </div>
       </header>
     </div>
   );
