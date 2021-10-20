a5995f0bab26aebb8a73df28a1abb47e03cdbbce
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-20 10:37:03 +0900

    📝ui (인프런 John Ahn)

--- React/react-shop-test/src/pages/OrderPage/Type.js
+++ React/react-shop-test/src/pages/OrderPage/Type.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Products from './Products';
 import ErrorBanner from '../../components/ErrorBanner';
+import Options from './Options';
 function Type({ orderType }) {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
@@ -22,7 +23,7 @@ function Type({ orderType }) {
     return <ErrorBanner message="에러가 발생했습니다." />;
   }
 
-  const ItemComponents = orderType === 'products' ? Products : null;
+  const ItemComponents = orderType === 'products' ? Products : Options;
 
   const optionItems = items.map((item) => (
     <ItemComponents
@@ -31,7 +32,21 @@ function Type({ orderType }) {
       imagePath={item.imagePath}
     />
   ));
-  return <div>{optionItems}</div>;
+  return (
+    <React.Fragment>
+      <h2>주문 종류</h2>
+      <p>하나의 가격</p>
+      <p>총 가격:</p>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: orderType === 'options' && 'column',
+        }}
+      >
+        {optionItems}
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default Type;
