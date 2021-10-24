5bb2578dfee96f4c5683006fd0586391bc0906f9
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-24 18:56:53 +0900

    📝옵션 가격을 위한 테스트 (인프런 John Ahn)

--- React/react-shop-test/src/pages/OrderPage/Type.js
+++ React/react-shop-test/src/pages/OrderPage/Type.js
@@ -38,11 +38,15 @@ function Type({ orderType }) {
       }
     />
   ));
+
+  let orderTypeKorean = orderType === 'products' ? '상품' : '옵션';
   return (
     <React.Fragment>
       <h2>주문 종류</h2>
       <p>하나의 가격</p>
-      <p>총 가격: {orderDatas.totals[orderType]}</p>
+      <p>
+        {orderTypeKorean} 총 가격: {orderDatas.totals[orderType]}
+      </p>
       <div
         style={{
           display: 'flex',
--- React/react-shop-test/src/pages/OrderPage/tests/calculate.test.js
+++ React/react-shop-test/src/pages/OrderPage/tests/calculate.test.js
@@ -5,7 +5,7 @@ import Type from '../Type';
 test("update product's total when products change", async () => {
   render(<Type orderType="products" />);
 
-  const productsTotal = screen.getByText('총 가격:', { exact: false });
+  const productsTotal = screen.getByText('상품 총 가격:', { exact: false });
   expect(productsTotal).toHaveTextContent('0');
 
   const americaInput = await screen.findByRole('spinbutton', {
@@ -15,4 +15,26 @@ test("update product's total when products change", async () => {
   userEvent.clear(americaInput);
   userEvent.type(americaInput, '1');
   expect(productsTotal).toHaveTextContent('1000');
+});
+
+test("update option's total when options change", async () => {
+  render(<Type orderType="options" />);
+
+  const optionsTotal = screen.getByText('옵션 총 가격:', { exact: false });
+  expect(optionsTotal).toHaveTextContent('0');
+
+  const insuranceCheckBox = await screen.findByRole('checkbox', {
+    name: 'Insurance',
+  });
+  userEvent.click(insuranceCheckBox);
+  expect(optionsTotal).toHaveTextContent('500');
+
+  const dinnerCheckbox = await screen.findByRole('checkbox', {
+    name: 'Dinner',
+  });
+  userEvent.click(dinnerCheckbox);
+  expect(optionsTotal).toHaveTextContent('1000');
+
+  userEvent.click(dinnerCheckbox);
+  expect(optionsTotal).toHaveTextContent('500');
 });
