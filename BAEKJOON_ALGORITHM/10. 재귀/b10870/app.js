ab8b5b9ac5be9e35901c1356c3b0415c63010364
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-30 20:48:19 +0900

    📝백준10870(팩토리얼)

---
+++ BAEKJOON_ALGORITHM/10. 재귀/b10870/app.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString();
+
+function f(n) {
+  if (n === 0) return 0;
+  else if (n === 1) return 1;
+  else return f(n - 1) + f(n - 2);
+}
+
+console.log(f(input));
