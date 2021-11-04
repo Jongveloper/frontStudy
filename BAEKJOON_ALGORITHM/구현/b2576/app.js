162f88a3dc193f64e37abb035e2ecf6d1b0890f8
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-11-04 16:39:41 +0900

    📝백준2576(홀수)

---
+++ BAEKJOON_ALGORITHM/구현/b2576/app.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n')
+  .map((item) => +item);
+
+let arr = [];
+for (let i = 0; i < input.length; i++) {
+  if (+input[i] % 2 !== 0) arr.push(+input[i]);
+}
+let sum = arr.reduce((a, b) => a + b);
+let min = Math.min.apply(null, arr);
+let answer = [];
+answer.push(sum, min);
+console.log(answer[0]);
+console.log(answer[1]);
