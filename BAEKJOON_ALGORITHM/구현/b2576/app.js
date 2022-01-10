196f455735bfe1ccb1d809db644b152dca867030
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2022-01-10 23:15:54 +0900

    📝백준2576(홀수) 런타임 에러 해결

--- BAEKJOON_ALGORITHM/구현/b2576/app.js
+++ BAEKJOON_ALGORITHM/구현/b2576/app.js
@@ -11,9 +11,14 @@ let arr = [];
 for (let i = 0; i < input.length; i++) {
   if (+input[i] % 2 !== 0) arr.push(+input[i]);
 }
-let sum = arr.reduce((a, b) => a + b);
-let min = Math.min.apply(null, arr);
-let answer = [];
-answer.push(sum, min);
-console.log(answer[0]);
-console.log(answer[1]);
+
+if (arr.length === 0) {
+  console.log(-1);
+} else {
+  let sum = arr.reduce((a, b) => a + b);
+  let min = Math.min.apply(null, arr);
+  let answer = [];
+  answer.push(sum, min);
+  console.log(answer[0]);
+  console.log(answer[1]);
+}
