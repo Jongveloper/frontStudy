ebe247c3cad25fe68d0e9acbcfb8398eaa2e1460
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2022-02-14 22:42:17 +0900

    :memo: (복습)자바스크립트 알고리즘 문제풀기(인프런 김태원님 강의)

---
+++ inflearn_javascript-algorithm/2022/section3/05/app.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const filePath = './input.txt';
+let input = fs.readFileSync(filePath).toString();
+
+function solution(s) {
+  let answer = '';
+  let cnt = 1;
+  s = s + ' ';
+  for (let i = 0; i < s.length - 1; i++) {
+    if (s[i] === s[i + 1]) cnt++;
+    else {
+      answer += s[i];
+      if (cnt > 1) answer += String(cnt);
+      cnt = 1;
+    }
+  }
+  return answer;
+}
+
+console.log(solution(input));
