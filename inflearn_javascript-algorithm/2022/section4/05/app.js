a7cfbb5f5fdbc8668d9d8149f2997b0b3b9988db
Woo Dy <woody@Wooui-MacBookPro.local>
2022-02-24 23:17:18 +0900

    :memo: (복습)자바스크립트 알고리즘 문제풀기(인프런 김태원님 강의)

---
+++ inflearn_javascript-algorithm/2022/section4/05/app.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const filePath = './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+let N = +input.shift();
+input = input
+  .toString()
+  .split(' ')
+  .map((item) => +item);
+
+function solution(arr) {
+  let answer = 0;
+  let max = Number.MIN_SAFE_INTEGER;
+  for (let i = 0; i < N; i++) {
+    let sum = input[i]
+      .toString()
+      .split('')
+      .map((item) => +item)
+      .reduce((a, b) => a + b);
+    if (sum >= max) {
+      max = sum;
+      if (input[i] > answer) answer = input[i];
+    }
+  }
+  return answer;
+}
+
+console.log(solution(input));
