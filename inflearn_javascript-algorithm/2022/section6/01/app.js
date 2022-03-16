53476454b6103e66ae3ca37cc14ca2c98916faaf
Woo Dy <kjop7419@naver.com>
2022-03-16 23:29:46 +0900

    :memo: (복습)자바스크립트 알고리즘 문제풀기(인프런 김태원님 강의) as 전

---
+++ inflearn_javascript-algorithm/2022/section6/01/app.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const filePath = './input.txt';
+let input = fs.readFileSync(filePath).toString();
+
+function solution(str) {
+  let answer = 'YES';
+  const stack = [];
+  for (let x of str) {
+    if (x === '(') stack.push(x);
+    else {
+      if (stack.length === 0) return 'NO';
+      stack.pop();
+    }
+  }
+  if (stack.length > 0) return 'NO';
+  return answer;
+}
+
+console.log(solution(input));
