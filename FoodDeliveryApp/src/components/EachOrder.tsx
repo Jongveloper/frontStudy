a1255162eefb600e3aabd0c8d4f586539175d58e
Woo Dy <kjop7419@naver.com>
2022-03-18 23:08:03 +0900

    :zap: 지도

--- FoodDeliveryApp/src/components/EachOrder.tsx
+++ FoodDeliveryApp/src/components/EachOrder.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from 'react'
-import { Alert, Pressable, Text, View } from "react-native";
+import { Alert, Pressable, Text, View, Dimensions } from "react-native";
 import orderSlice, { Order } from "../slices/order";
 import { StyleSheet } from "react-native";
 import { useAppDispatch } from "../store";
@@ -9,14 +9,15 @@ import { useSelector } from "react-redux";
 import { RootState } from "../store/reducer";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { LoggedInParamList } from "../../AppInner";
-import EncryptedStorage from "react-native-encrypted-storage";
-
+import NaverMapView, { Marker, Path } from "react-native-nmap";
+import getDistanceFromLatLonInKm from "../util";
 function EachOrder({ item }: { item: Order }) {
   const dispatch = useAppDispatch();
   const navigation = useNavigation<NavigationProp<LoggedInParamList>>();
   const [detail, setDetail] = useState(false);
   const [loading, setLoading] = useState(false);
   const accessToken = useSelector((state: RootState) => state.user.accessToken);
+  const { start, end } = item;
   const toggleDetail = useCallback(() => {
     setDetail(prev => !prev)
   }, []);
@@ -48,16 +49,58 @@ function EachOrder({ item }: { item: Order }) {
   return (
     <View key={item.orderId} style={styles.orderContainer}>
       <Pressable onPress={toggleDetail} style={styles.info}>
-        <Text>
+        <Text style={styles.eachInfo}>
           {item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원
+        </Text>
+        <Text style={styles.eachInfo}>
+          {getDistanceFromLatLonInKm(
+            start.latitude,
+            start.longitude,
+            end.latitude,
+            end.longitude,
+          ).toFixed(1)}
+          km
         </Text>
         <Text>왕십리</Text>
         <Text>면목동</Text>
       </Pressable>
       {detail ?
         <View>
-          <View>
-            <Text>네이버 맵이 들어갈 장소</Text>
+          <View
+            style={{
+              width: Dimensions.get('window').width - 30,
+              height: 200,
+              marginTop: 10,
+            }}>
+            <NaverMapView
+              style={{ width: '100%', height: '100%' }}
+              zoomControl={false}
+              center={{
+                zoom: 10,
+                tilt: 50,
+                latitude: (start.latitude + end.latitude) / 2,
+                longitude: (start.longitude + end.longitude) / 2,
+              }}>
+              <Marker
+                coordinate={{
+                  latitude: start.latitude,
+                  longitude: start.longitude,
+                }}
+                pinColor="blue"
+              />
+              <Path
+                coordinates={[
+                  {
+                    latitude: start.latitude,
+                    longitude: start.longitude,
+                  },
+                  { latitude: end.latitude, longitude: end.longitude },
+                ]}
+              />
+              <Marker
+                coordinate={{ latitude: end.latitude, longitude: end.longitude }}
+              />
+            </NaverMapView>
           </View>
           <View style={styles.buttonWrapper}>
             <Pressable onPress={onAccept} disabled={loading} style={styles.acceptButton}>
@@ -83,7 +126,9 @@ const styles = StyleSheet.create({
   },
   info: {
     flexDirection: 'row',
-    justifyContent: 'space-between'
+  },
+  eachInfo: {
+    flex: 1,
   },
   buttonWrapper: {
     flexDirection: 'row',
