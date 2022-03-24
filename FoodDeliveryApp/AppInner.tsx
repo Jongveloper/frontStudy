36465743605eeb05341bac7a924310fe4cd5c681
Woo Dy <kjop7419@naver.com>
2022-03-24 22:52:38 +0900

    :art: 아이콘 적용

--- FoodDeliveryApp/AppInner.tsx
+++ FoodDeliveryApp/AppInner.tsx
@@ -20,6 +20,8 @@ import userSlice from "./src/slices/user";
 import { Alert } from "react-native";
 import usePermissions from "./src/hooks/usePermissions";
 import SplashScreen from "react-native-splash-screen";
+import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
 export type LoggedInParamList = {
   Orders: undefined;
@@ -125,17 +127,17 @@ function AppInner() {
       <Tab.Screen
         name="Orders"
         component={Orders}
-        options={{ title: '오더 목록' }}
+        options={{ title: '오더 목록', tabBarIcon: () => <FontAwesome5Icon name="list" size={20} /> }}
       />
       <Tab.Screen
         name="Delivery"
         component={Delivery}
-        options={{ title: '내 오더' }}
+        options={{ title: '지도', tabBarIcon: () => <FontAwesome5Icon name="map" size={20} /> }}
       />
       <Tab.Screen
         name="Settings"
         component={Settings}
-        options={{ title: '내 정보' }}
+        options={{ title: '내 정보', tabBarIcon: () => <FontAwesomeIcon name="gear" size={20} /> }}
       />
     </Tab.Navigator>
   ) : (
