f6839c53235abd65e657b2b567a940a3906e7410
Woo Dy <kjop7419@naver.com>
2022-03-29 21:32:32 +0900

    :memo: (복습)자바스크립트 알고리즘 문제풀기(인프런 김태원님 강의)

---
+++ inflearn_javascript-algorithm/2022/section7/03/app.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const filePath = './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+let n = +input.shift();
+input = input
+  .toString()
+  .split(' ')
+  .map((item) => +item);
+
+function solution(n, arr) {
+  let answer = arr;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > 0 && arr[j + 1] < 0) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return answer;
+}
+
+console.log(solution(n, input));
