3b65c0f3322d595cb7047216baf00e3600bf07f7
Woo Dy <kjop7419@naver.com>
2022-04-25 21:18:19 +0900

    :lipstick: 회원가입 페이지 뷰

--- sleact/lecture/layouts/App.tsx
+++ sleact/lecture/layouts/App.tsx
@@ -1,13 +1,16 @@
 import React, { FC } from "react";
-import { Routes, Route } from 'react-router-dom'
+import loadable from '@loadable/component';
+import { Routes, Route } from 'react-router-dom';
 import { Navigate } from "react-router";
-import Login from '@pages/LogIn'
-import SignUp from '@pages/SignUp'
+
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
+
 const App: FC = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigate replace to='/login' />} />
-      <Route path='/login' element={<Login />} />
+      <Route path='/login' element={<LogIn />} />
       <Route path='/signup' element={<SignUp />} />
     </Routes>
   )
