73bb85682fba43621824afe1d7704387badb9ba5
Woo Dy <kjop7419@naver.com>
2022-04-25 20:27:57 +0900

    :zap: react-router

--- sleact/lecture/client.tsx
+++ sleact/lecture/client.tsx
@@ -1,6 +1,6 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { render } from "react-dom";
+import App from '@layouts/App';
 
-import App from './layouts/App';
-
-render(<App />, document.querySelector('#app'));
+render(<Router><App /></Router>, document.querySelector('#app'));
--- sleact/lecture/layouts/App.tsx
+++ sleact/lecture/layouts/App.tsx
@@ -1,8 +1,15 @@
-import React from "react";
-
-const App = () => {
+import React, { FC } from "react";
+import { Routes, Route } from 'react-router-dom'
+import { Navigate } from "react-router";
+import Login from '@pages/LogIn'
+import SignUp from '@pages/SignUp'
+const App: FC = () => {
   return (
-    <div>세팅 진행중..</div>
+    <Routes>
+      <Route path='/' element={<Navigate replace to='/login' />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<SignUp />} />
+    </Routes>
   )
 }
 
