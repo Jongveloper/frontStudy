5cfb884ebe4db823ee7395da8c1cef27532b3fb4
Woo Dy <kjop7419@naver.com>
2022-04-30 19:19:43 +0900

    :zap: customScrollbars&&dayjs

--- sleact/lecture/components/ChatList/index.tsx
+++ sleact/lecture/components/ChatList/index.tsx
@@ -1,19 +1,25 @@
 import { ChatZone, Section } from '@components/ChatList/styles';
 import { IDM } from "@typings/db";
-import React, { FC } from 'react';
+import React, { FC, useCallback, useRef } from 'react';
 import Chat from '@components/Chat'
+import { Scrollbars } from 'react-custom-scrollbars';
 
 interface Props {
   chatData?: IDM[];
 }
 
 const ChatList: FC<Props> = ({ chatData }) => {
+  const scrollbarRef = useRef(null);
+  const onScroll = useCallback(() => {
 
+  }, [])
   return (
     <ChatZone>
-      {chatData?.map((chat) => (
-        <Chat key={chat.id} data={chat} />
-      ))}
+      <Scrollbars ref={scrollbarRef} onScrollFrame={onScroll}>
+        {chatData?.map((chat) => (
+          <Chat key={chat.id} data={chat} />
+        ))}
+      </Scrollbars>
     </ChatZone>
   );
 };
